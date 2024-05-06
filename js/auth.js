6c881e1e1bc4051343af08e3cf562c3624ed00fc
Ahmed Barakat <107679419+ahmedBarakat310@users.noreply.github.com>
2024-05-06 23:57:14 +0300

    Add files via upload

---
+++ js/auth.js
@@ -0,0 +1,107 @@
+let x = "";
+
+function setDefultUsers() {
+  let users0 = [{ username: "admin", password: "0000" }];
+  if (localStorage.getItem("users") == null || localStorage.getItem("users")== []) {
+    localStorage.setItem("users", JSON.stringify(users0));
+   
+  } else {
+    users0 = JSON.parse(localStorage.getItem("users"));
+
+    localStorage.setItem("users", JSON.stringify(users0));
+
+  }
+}
+
+
+// get username and password if you for get it
+let forgetPassword = document.getElementById("forgetPassword");
+forgetPassword.addEventListener("click", () => {
+  alert("UserName:Admin,Password:0000, no correct way");
+});
+
+// set defult user to localstorage 
+
+// control with login button to work his function
+
+var loginFormAnchor = document.querySelector("#loginForm a");
+loginFormAnchor.addEventListener("click", validateLoginForm);
+function validateLoginForm() {
+  var username = document.getElementById("loginUsername").value;
+  var password = document.getElementById("loginPassword").value;
+  var errorDiv = document.getElementById("loginError");
+setDefultUsers()
+
+
+  errorDiv.innerHTML = "";
+
+  if (username.trim() === "") {
+    errorDiv.innerHTML = "Please enter your username";
+    return false;
+  }
+
+  if (password.trim() === "") {
+    errorDiv.innerHTML = "Please enter your password";
+    return false;
+  }
+
+  var users = JSON.parse(localStorage.getItem("users")) || [];
+  var foundUser = users.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (!foundUser) {
+    errorDiv.innerHTML = "Invalid username or password";
+    return false;
+  }
+  loginFormAnchor.href = "home.html"
+  return true;
+}
+
+var SignUpFormAnchor = document.getElementById("#regBtn");
+
+function validateRegisterForm() {
+  var username = document.getElementById("registerUsername").value;
+  var email = document.getElementById("registerEmail").value;
+  var password = document.getElementById("registerPassword").value;
+  var errorDiv = document.getElementById("registerError");
+  errorDiv.innerHTML = "";
+  errorDiv.classList.remove("green");
+
+  if (username.trim() === "") {
+    errorDiv.innerHTML = "Please enter a username";
+    return false;
+  }
+
+  if (email.trim() === "") {
+    errorDiv.innerHTML = "Please enter an email";
+    return false;
+  } else if (!validateEmail(email)) {
+    errorDiv.innerHTML = "Please enter a valid email address";
+    return false;
+  }
+
+  if (password.trim() === "") {
+    errorDiv.innerHTML = "Please enter a password";
+    return false;
+  }
+
+  var users = JSON.parse(localStorage.getItem("users")) || [];
+  var existingUser = users.find((user) => user.username === username);
+  if (existingUser) {
+    errorDiv.innerHTML = "Username already exists";
+    return false;
+  }
+
+  users.push({ username: username, email: email, password: password });
+  localStorage.setItem("users", JSON.stringify(users));
+
+  errorDiv.innerHTML = "Registration successful!";
+  errorDiv.classList.add("green");
+
+  return true;
+}
+
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
---
+++ js/main.js
@@ -0,0 +1,204 @@
+let x = 0;
+
+
+
+let tableSettings = document.getElementById("tableSettings");
+function ShowUsers() {
+  let user = ``;
+
+  if (localStorage.getItem('users') != null) {
+     let users = JSON.parse(localStorage.getItem('users'));
+     
+     for (let i = 0; i < users.length; i++) {
+       
+        let hideFirstButton = i === 0 ? 'style=" pointer-events:none;"' : ''; // Check if it's the first row
+   
+        user += `
+        <tr>
+          <td>${users[i].username}</td>
+          <td>${users[i].password}</td>
+          <td><button class="deleteBtn" ${hideFirstButton} onclick="DeleteElement(${i})">Delete</button></td>
+        </tr>
+        `;
+        tableSettings.innerHTML = user;
+     }
+  
+  } else {
+     console.log('there are no users');
+  }
+}
+
+// active navigation to open and close
+let list = document.querySelectorAll(".navigation li");
+function activeLink() {
+  list.forEach((item) => item.classList.remove("hovered"));
+  this.classList.add("hovered");
+}
+list.forEach((item) => item.addEventListener("mouseover", activeLink));
+let navigation = document.querySelector(".navigation");
+let toggle = document.querySelector(".toggle");
+let main = document.querySelector(".main");
+toggle.addEventListener("click", () => {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+});
+
+
+// gwt user from localstorage
+let allUser = [];
+if (localStorage.getItem("users") != null) {
+  allUser = JSON.parse(localStorage.getItem("users"));
+}
+// delete user accces
+function DeleteElement(index) {
+  allUser.splice(index, 1);
+
+  localStorage.setItem("users", JSON.stringify(allUser));
+  ShowUsers();
+}
+
+ShowUsers();
+
+// networkData = [
+//   {
+//     centerName: "It Department",
+//     ipCenter: "192.168.1.0",
+//     connectedDevice: [
+//       { namee: "HOD CABIN", ip: "192.186.1.2" },
+//       { namee: "IT LAB1", ip: "192.168.1.3" },
+//       { namee: "IT LAB2", ip: "192.168.1.4" },
+//       { namee: "IT LAB3", ip: "192.168.1.5" },
+//       { namee: "IT LAB4", ip: "192.168.1.6" },
+//       { namee: "Printer0", ip: "192.168.1.7" },
+//     ],
+//   },
+//   {
+//     centerName: "Computer Department",
+//     ipCenter: "192.168.2.0",
+//     connectedDevice: [
+//       { namee: "CS HOD CABIN", ip: "192.168.2.2" },
+
+//       { namee: "CSLAB1", ip: "192.168.2.3" },
+//       { namee: "CSLAB2", ip: "192.168.2.4" },
+//       { namee: "CSLAB3", ip: "192.168.2.5" },
+//       { namee: "CSLAB4", ip: "192.168.2.6" },
+//       { namee: "Printer7", ip: "192.168.2.7" },
+//     ],
+//   },
+//   {
+//     centerName: "Others",
+//     ipCenter: "192.168.3.0",
+//     connectedDevice: [
+//       { namee: "OFFICE", ip: "192.168.3.2" },
+
+//       { namee: "Printer2", ip: "192.168.3.6" },
+//       { namee: "EXAM CELL", ip: "192.168.3.3" },
+//       { namee: "Printer3", ip: "192.168.3.7" },
+//       { namee: "ENQUIRY", ip: "192.168.3.4" },
+//       { namee: "TPO", ip: "192.168.3.5" },
+//       { namee: "Printer4", ip: "192.168.3.8" },
+//     ],
+//   },
+//   {
+//     centerName: "Server Room",
+//     ipCenter: "1.0.0.0",
+//     connectedDevice: [
+//       { namee: "FTP SERVER", ip: "1.0.0.4" },
+
+//       { namee: "PC1", ip: "1.0.0.5" },
+//       { namee: "DNS SERVER", ip: "1.0.0.2" },
+//       { namee: "WEB SERVER", ip: "1.0.0.3" }
+      
+//     ],
+//   },
+//   {
+//    centerName: "Internet Lab",
+//    ipCenter: "192.168.0.0",
+//    connectedDevice: [
+
+//      { namee: "PC2", ip: "192.168.0.2" },
+//      { namee: "PC3 ", ip: "192.168.0.3" },
+//      { namee: "PC4", ip: "192.168.0.4" },
+//      { namee: "PC5", ip: "192.168.0.5" },
+//      { namee: "Printer5", ip: "192.168.0.6" },
+//    ],
+//  },
+//  {
+//    centerName: "Principle Lab",
+//    ipCenter: "192.168.4.0",
+//    connectedDevice: [
+
+//      { namee: "PC0", ip: "192.168.4.2" },
+//      { namee: "LabTop0 ", ip: "192.168.4.3" },
+   
+//    ],
+//  },
+// ];
+// let ipContainer=document.getElementById('ipContainer');
+// let ipDevices=document.getElementById('ipDevices');
+
+// let collectionOFIp=` `;
+// for (let i = 0; i < networkData.length; i++){
+
+// let collectionOFIpDevices=``;
+//    for (let j = 0; j < networkData[i].connectedDevice.length; j++) {
+//     collectionOFIpDevices +=`
+//     <li>${networkData[i].connectedDevice[j].namee} (<span>${networkData[i].connectedDevice[j].ip}</span>) </li>
+
+//     `
+
+      
+//    }
+//    collectionOFIp +=`
+//    <div class="ipBox">
+//    <h2>${networkData[i].centerName} (<span>${networkData[i].ipCenter}</span> )</h2>
+//    <ul  id="ipDevices">
+   
+//    ${collectionOFIpDevices}
+     
+     
+//    </ul>
+// </div>
+//    `
+
+   
+// console.log(collectionOFIp);
+//      ipContainer.innerHTML=collectionOFIp
+   
+//   }
+
+// This function creates a default user if no users are in local storage
+// function createDefaultUser() {
+//   if (localStorage.getItem("users") === null) {
+//     localStorage.setItem('users', JSON.stringify([{ username: "admin", password: "0000" }]));
+//   }
+// }
+
+// // // This function displays the users in the table
+// function ShowUsers() {
+//   createDefaultUser(); // Ensure there is at least one user
+
+//   let userHTML = '';
+
+//   if (localStorage.getItem("users") != null) {
+//     let users = JSON.parse(localStorage.getItem("users"));
+
+//     users.forEach((user, i) => {
+//       let hideFirstButton = i === 0 ? 'style="pointer-events:none; opacity:0.4;"' : "";
+//       userHTML += `
+//         <tr>
+//           <td>${user.username}</td>
+//           <td>${user.password}</td>
+//           <td><button class="deleteBtn" ${hideFirstButton} onclick="DeleteElement(${i})">Delete</button></td>
+//         </tr>`;
+//     });
+//   } else {
+//     // If no users are found, display a message
+//     userHTML = `
+//       <tr>
+//         <td colspan="3">No users found</td>
+//       </tr>`;
+//   }
+
+//   tableSettings.innerHTML = userHTML; // Set the table's inner HTML after constructing all user rows
+// }
